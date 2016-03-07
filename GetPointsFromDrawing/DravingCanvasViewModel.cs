43c36fc642d8ae782a642221b450ecae1516f90f
PFalkowski <piotr.falkowski.fm@gmail.com>
2016-03-07 19:18:30 +0100

    Add working drawing canvas, unfortunatly no eas way to implement drawing other than by events

---
+++ GetPointsFromDrawing/DravingCanvasViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GeoLib;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace DravingCanvas
+{
+    public class DravingCanvasViewModel : BindableBase
+    {
+        public bool drawingIsOn { get; set; }
+        public Point startPoint { get; set; }
+        public ObservableCollection<C2DPoint> Points { get; set; } = new ObservableCollection<C2DPoint>();
+
+    }
+}
---
+++ GetPointsFromDrawing/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DravingCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DravingCanvasViewModel viewModel;
+        //private Polygon selectedPolygon { get; set; } = new Polygon { Stroke = Brushes.Gray, StrokeThickness = 1, StrokeDashArray = new DoubleCollection { 3, 3 } };
+        //public bool containerEmpty => selectedPolygon.Points.Count == 0;
+        public Point startPoint { get; set; }
+        private bool firstPoint = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new DravingCanvasViewModel();
+            DataContext = viewModel;
+        }
+        
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //selectedPolygon.Points.Add(e.GetPosition(canvas));
+            if (firstPoint)
+            {
+                startPoint = e.GetPosition(canvas);
+                firstPoint = false;
+                viewModel.Points.Add(new GeoLib.C2DPoint(startPoint.X, startPoint.Y));
+                return;
+            }
+            var pathTravelled = new Line();
+            pathTravelled.Stroke = Brushes.Black;
+            pathTravelled.StrokeThickness = 2;
+            var finishingPoint = e.GetPosition(canvas);
+            pathTravelled.X1 = startPoint.X;
+            pathTravelled.Y1 = startPoint.Y;
+            pathTravelled.X2 = finishingPoint.X;
+            pathTravelled.Y2 = finishingPoint.Y;
+            canvas.Children.Add(pathTravelled);
+            startPoint = e.GetPosition(canvas);
+            viewModel.Points.Add(new GeoLib.C2DPoint(finishingPoint.X, finishingPoint.Y));
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            //selectedPolygon = new Polygon { Stroke = Brushes.Gray, StrokeThickness = 1, StrokeDashArray = new DoubleCollection { 3, 3 } };
+            firstPoint = true;
+            canvas.Children.Clear();
+            viewModel.Points.Clear();
+            //canvas.Children.Add(selectedPolygon);
+        }
+    }
+}
