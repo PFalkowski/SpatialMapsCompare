d4cb97520b0e13101c29d92907fd4265d9fb20ce
Piotr <piotr.falkowski.fm@gmail.com>
2016-08-05 00:05:53 +0200

    Add unit test classes

---
+++ SpatialMapsCompareTests/SpatialMapsModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SpatialMaps;
+
+namespace SpatialMapsCompareTests
+{
+    [TestClass]
+    public class SpatialMapsModelTests
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var moq = new Mock<IOService>(MockBehavior.Strict);
+            var model = new SpatialMapsModel(moq.Object);
+            Assert.IsTrue(model.FileType.Contains("xml"));
+            Assert.IsTrue(model.FilterString.Contains(model.FileType));
+            Assert.IsTrue(model.RoundDigits < 5);
+        }
+        [TestMethod]
+        public void IsPolygonValidTest()
+        {
+            var model = new SpatialMapsModel(null);
+            Assert.IsFalse(model.IsPolygonValid(null));
+        }
+    }
+}
---
+++ SpatialMapsCompareTests/SpatialMapsViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpatialMapsCompareTests
+{
+    [TestClass]
+    public class SpatialMapsViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
